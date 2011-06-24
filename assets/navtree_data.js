12463644b6c9f32e7b7c1a68b3dedfb84faa5719
Emiliano Lesende <emiliano.lesende@gmail.com>
2011-06-24 12:54:59 -0300

    Added JavaDoc

---
+++ assets/navtree_data.js
@@ -0,0 +1,10 @@
+var NAVTREE_DATA =
+[ [ "org.mule.devkit.annotations", "org/mule/devkit/annotations/package-summary.html", [ [ "Description", "org/mule/devkit/annotations/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "SourceCallback", "org/mule/devkit/annotations/SourceCallback.html", null, "" ] ]
+, "" ], [ "Annotations", null, [ [ "Configurable", "org/mule/devkit/annotations/Configurable.html", null, "" ], [ "Module", "org/mule/devkit/annotations/Module.html", null, "" ], [ "Parameter", "org/mule/devkit/annotations/Parameter.html", null, "" ], [ "Processor", "org/mule/devkit/annotations/Processor.html", null, "" ], [ "Source", "org/mule/devkit/annotations/Source.html", null, "" ], [ "Transformer", "org/mule/devkit/annotations/Transformer.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.devkit.annotations.lifecycle", "org/mule/devkit/annotations/lifecycle/package-summary.html", [ [ "Description", "org/mule/devkit/annotations/lifecycle/package-descr.html", null, "" ], [ "Annotations", null, [ [ "Dispose", "org/mule/devkit/annotations/lifecycle/Dispose.html", null, "" ], [ "Initialise", "org/mule/devkit/annotations/lifecycle/Initialise.html", null, "" ], [ "Start", "org/mule/devkit/annotations/lifecycle/Start.html", null, "" ], [ "Stop", "org/mule/devkit/annotations/lifecycle/Stop.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ reference/lists.js
@@ -0,0 +1,16 @@
+var DATA = [
+      { id:0, label:"org.mule.devkit.annotations", link:"org/mule/devkit/annotations/package-summary.html", type:"package" },
+      { id:1, label:"org.mule.devkit.annotations.Configurable", link:"org/mule/devkit/annotations/Configurable.html", type:"class" },
+      { id:2, label:"org.mule.devkit.annotations.Module", link:"org/mule/devkit/annotations/Module.html", type:"class" },
+      { id:3, label:"org.mule.devkit.annotations.Parameter", link:"org/mule/devkit/annotations/Parameter.html", type:"class" },
+      { id:4, label:"org.mule.devkit.annotations.Processor", link:"org/mule/devkit/annotations/Processor.html", type:"class" },
+      { id:5, label:"org.mule.devkit.annotations.Source", link:"org/mule/devkit/annotations/Source.html", type:"class" },
+      { id:6, label:"org.mule.devkit.annotations.SourceCallback", link:"org/mule/devkit/annotations/SourceCallback.html", type:"class" },
+      { id:7, label:"org.mule.devkit.annotations.Transformer", link:"org/mule/devkit/annotations/Transformer.html", type:"class" },
+      { id:8, label:"org.mule.devkit.annotations.lifecycle", link:"org/mule/devkit/annotations/lifecycle/package-summary.html", type:"package" },
+      { id:9, label:"org.mule.devkit.annotations.lifecycle.Dispose", link:"org/mule/devkit/annotations/lifecycle/Dispose.html", type:"class" },
+      { id:10, label:"org.mule.devkit.annotations.lifecycle.Initialise", link:"org/mule/devkit/annotations/lifecycle/Initialise.html", type:"class" },
+      { id:11, label:"org.mule.devkit.annotations.lifecycle.Start", link:"org/mule/devkit/annotations/lifecycle/Start.html", type:"class" },
+      { id:12, label:"org.mule.devkit.annotations.lifecycle.Stop", link:"org/mule/devkit/annotations/lifecycle/Stop.html", type:"class" }
+
+    ];
