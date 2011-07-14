3226effd6bc25689759bcb4dc6afc4d2851ad1e7
Emiliano Lesende <emiliano.lesende@gmail.com>
2011-07-14 10:32:01 -0300

    Added new ProcessorCallback interface

---
+++ core/annotations/src/main/java/org/mule/api/annotations/callback/ProcessorCallback.java
@@ -0,0 +1,32 @@
+/**
+ * Mule Development Kit
+ * Copyright 2010-2011 (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mule.api.annotations.callback;
+
+/**
+ * Callback interface used by {@link org.mule.api.annotations.Processor} annotated methods if they want to
+ * receive nested processors within them.
+ */
+public interface ProcessorCallback {
+    /**
+     * Dispatch message to the processor chain
+     *
+     * @param payload The payload of the message
+     * @return The return payload for the processor chain
+     */
+    Object process(Object payload);
+}
