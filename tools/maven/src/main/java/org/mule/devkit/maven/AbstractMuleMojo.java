6bd226ef5539941fd15b20b10134b8a881e60c6a
Federico Recio <federico.recio@mulesoft.com>
2012-01-02 12:14:48 -0300

    by default do not generate Mule Studio plugin

--- tools/maven/src/main/java/org/mule/devkit/maven/AbstractMuleMojo.java
+++ tools/maven/src/main/java/org/mule/devkit/maven/AbstractMuleMojo.java
@@ -53,7 +53,7 @@ public abstract class AbstractMuleMojo extends AbstractMojo {
     /**
      * Whether to skip the creating of a Mule Studio plugin.
      */
-    @MojoParameter(expression = "${devkit.studio.package.skip}", defaultValue = "false")
+    @MojoParameter(expression = "${devkit.studio.package.skip}", defaultValue = "true")
     protected boolean skipStudioPluginPackage;
 
     protected File getMuleZipFile() {
